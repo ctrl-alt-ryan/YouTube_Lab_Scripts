67dc95789dad4f842a2c3ca9f91c621735f33bc6
Ctrl Alt Ryan <thegrishpack@gmail.com>
2026-03-16 15:57:41 -0500

    Update main.tf

--- Proxmox/K3s_Phase_2_Lab/Terraform/main.tf
+++ Proxmox/K3s_Phase_2_Lab/Terraform/main.tf
@@ -51,6 +51,6 @@ resource "proxmox_vm_qemu" "k3s_nodes" {
   ipconfig0  = "ip=${each.value.ip},gw=${each.value.gw}"
 
   sshkeys    = <<EOF
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCsX73iHuxvjUPar0kq8ma2Yc85/NDSwyXj72S/D748RsuEe1O42Mf0YKpg0dZffsxYmf7Ts3SPL0Q29rvlBT1PafGGNaToc2Rth3xIaOsmNJPI0eMlGPfMa7w1tUGBIZCUK1H2tu0uhgkDKcgAhSLVcKhMRQuYeichnWzBjOoOuam7pCCn2iG9XhCQScyZopCaHCNZGA7tXGL6Ji6EzazqDhjFcLgsRqLXOUMiZ0upUBQDAdjXHsKmosTXVggDee5d+EbXv5k94x7t4Zr/qpwRsNkjHvBYVmRjlfl2bYWE7zn4Exb4In9SB6DVbZGsIradkhIqXsCfEbLUQCHx7VSVLK0wxIXhKjWfHk/dZ7FGx3Rkphqti9cbX5sgqXptqlfgBnqXJr2tCVkR3uAx9JiHxQkHrTbL5xBlMLbz1voRfZNDeE5ZdLx4WSgcjc1z9hk7Xwhyi+Oaq0hZXmHVyz/5/FWA5btJisEPPTa1hjTURcHvFQE5yVgsAk+QfThChLN+3ognRADcKHlg7hwf1fZ2IBG2lAOjgko6ATCe2/R+5WbYJSAINBk4pD+q+Otbkhy522vCKa0yfAnNXVoNourOy/QEbCQi7XTpYC3lYb+JUY9HEjwTlbFGocSSs1aFKLhfBynLxRGAkm7kI9gPxjyy17Jgy8P5QBBD/IG6SYZzJQ== ryan@RG-Fedora-S1
+YOUR-SSH-KEYS
 EOF
 }
